ff806700eea5119c72ed41bd8a425621efc946d8
Tyler Higgins <tylermhiggins@outlook.com>
2021-10-11 21:16:39 -0500

    old

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
         uic.loadUi("cybervault.ui", self)
         self.show()
+
 
 app = QApplication([])
 window = UI()
