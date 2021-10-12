926d745586bd7da24617d9ae4b71c420734a36b2
Tyler Higgins <tylermhiggins@outlook.com>
2021-10-12 10:17:57 -0500

    Worked on Welcome Screen, create login, open vault and new account windows.

--- main.py
+++ main.py
@@ -1,15 +1,55 @@
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 
 
+# Done
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
         loadUi("cybervault.ui", self)
-        self.show()
+        self.new_account.clicked.connect(self.create_account)
+        self.open_cybervault.clicked.connect(self.open_vault)
+        self.login_to_account.clicked.connect(self.login)
+
+    def create_account(self):
+        newaccountwindow = New_User()
+        widget.addWidget(newaccountwindow)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def open_vault(self):
+        openvaultwindow = OpenCyberVault()
+        widget.addWidget(openvaultwindow)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def login(self):
+        loginwindow = Login()
+        widget.addWidget(loginwindow)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+class New_User(QDialog):
+    def __init__(self):
+        super(New_User, self).__init__()
+        loadUi("newaccount.ui", self)
+
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("login.ui", self)
+
+
+class OpenCyberVault(QDialog):
+    def __init__(self):
+        super(OpenCyberVault, self).__init__()
+        loadUi("opencybervault.ui", self)
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = UI()
+    widget = QtWidgets.QStackedWidget()
+    mainwindow = UI()
+    widget.addWidget(mainwindow)
+    widget.show()
     app.exec_()
