49e33817288037591e900432107e4f24a244129a
Tyler Higgins <tylermhiggins@outlook.com>
2021-10-12 11:13:51 -0500

    worked a little more on the new account window.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
+from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 
 
@@ -13,7 +13,7 @@ class UI(QMainWindow):
         self.login_to_account.clicked.connect(self.login)
 
     def create_account(self):
-        newaccountwindow = New_User()
+        newaccountwindow = NewUser()
         widget.addWidget(newaccountwindow)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
@@ -28,10 +28,28 @@ class UI(QMainWindow):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 
-class New_User(QDialog):
+class NewUser(QDialog):
     def __init__(self):
-        super(New_User, self).__init__()
+        super(NewUser, self).__init__()
         loadUi("newaccount.ui", self)
+
+        # Setting up on screen options
+        self.checked = None
+        self.enableMFA.stateChanged.connect(self.enable_mfa)
+        self.create_account_button.clicked.connect(self.create_account)
+
+    def enable_mfa(self):
+        self.checked = self.enableMFA.isChecked()
+
+    def create_account(self):
+        username = self.username.text()
+        if self.checked:
+            print("Is checked\n")
+
+            print(f"Username: {username}")
+        else:
+            print("No MFA\n")
+            print(f"Username: {username}")
 
 
 class Login(QDialog):
