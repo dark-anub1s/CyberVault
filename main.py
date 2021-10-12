cf4f6920a4b1802b8db9172f4939279f2ad19f28
Tyler Higgins <tylermhiggins@outlook.com>
2021-10-11 21:19:26 -0500

    Updated.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
@@ -10,13 +9,7 @@ class UI(QMainWindow):
         self.show()
 
 
-<<<<<<< HEAD
-app = QApplication([])
-window = UI()
-app.exec_()
-=======
 if __name__ == '__main__':
     app = QApplication([])
     window = UI()
     app.exec_()
->>>>>>> 277fad4031cd3e4a405965960048bd1adb6deada
